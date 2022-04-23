84acfe32b52707e275b5cd2d0175fe5bb3bae3f0
nour <ng9sc@virginia.edu>
2022-04-23 14:35:18 -0400

    Creating Pop-up modal

--- templates/jeopardy.php
+++ templates/jeopardy.php
@@ -10,6 +10,7 @@
     <title>Jeopardy</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/main.css">
+    <script type="text/javascript" src="templates/buttons.js"></script>
 </head>
 
 <body>
@@ -67,209 +68,182 @@
                 </div>
             </div>
 
-            <!-- API Question not showing up -->
-            <div onclick="popUp();">
-                <div class="row">
-                    <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                        <div class="well card-box bg-blue">
-                            <button class="boxes" id="myBtn">$200</button>
-                        </div>
+            <div class="row">
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue" onclick="popUp();">
+                        <button class="boxes" id="myBtn">$200</button>
                     </div>
-                    <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                        <div class="well card-box bg-blue">
-                            <button class="boxes" id="myBtn">$200</button>
-                        </div>
+                </div>
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue" onclick="popup();">
+                        <button class="boxes" id="myBtn1">$200</button>
                     </div>
-                    <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                        <div class="well card-box bg-blue">
-                            <button class="boxes" id="myBtn">$200</button>
-                        </div>
+                </div>
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn2">$200</button>
                     </div>
-                    <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                        <div class="well card-box bg-blue">
-                            <button class="boxes" id="myBtn">$200</button>
-                        </div>
+                </div>
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn3">$200</button>
                     </div>
-                    <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                        <div class="well card-box bg-blue">
-                            <button class="boxes" id="myBtn">$200</button>
-                        </div>
+                </div>
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn4">$200</button>
                     </div>
-        </div>
-        </div>
+                </div>
+            </div>
 
-        <div onclick="answerQuestion();">
             <div class="row">
                 <div class="box btn btn-fix text-center col-2 jeopardy-card">
                     <div class="well card-box bg-blue">
-                        <button class="boxes" id="myBtn">$400</button>
+                        <button class="boxes" id="myBtn5">$400</button>
                     </div>
                 </div>
                 <div class="box btn btn-fix text-center col-2 jeopardy-card">
                     <div class="well card-box bg-blue">
-                        <button class="boxes" id="myBtn">$400</button>
+                        <button class="boxes" id="myBtn6">$400</button>
                     </div>
                 </div>
                 <div class="box btn btn-fix text-center col-2 jeopardy-card">
                     <div class="well card-box bg-blue">
-                        <button class="boxes" id="myBtn">$400</button>
+                        <button class="boxes" id="myBtn7">$400</button>
                     </div>
                 </div>
                 <div class="box btn btn-fix text-center col-2 jeopardy-card">
                     <div class="well card-box bg-blue">
-                        <button class="boxes" id="myBtn">$400</button>
+                        <button class="boxes" id="myBtn8">$400</button>
                     </div>
                 </div>
                 <div class="box btn btn-fix text-center col-2 jeopardy-card">
                     <div class="well card-box bg-blue">
-                        <button class="boxes" id="myBtn">$400</button>
+                        <button class="boxes" id="myBtn9">$400</button>
                     </div>
                 </div>
+            </div>
+
+            <div class="row">
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn10">$600</button>
+                    </div>
+                </div>
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn11">$600</button>
+                    </div>
+                </div>
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn12">$600</button>
+                    </div>
+                </div>
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn13">$600</button>
+                    </div>
+                </div>
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn14">$600</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn15">$800</button>
+                    </div>
+                </div>
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn16">$800</button>
+                    </div>
+                </div>
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn17">$800</button>
+                    </div>
+                </div>
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn18">$800</button>
+                    </div>
+                </div>
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn19">$800</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn20">$1000</button>
+                    </div>
+                </div>
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn21">$1000</button>
+                    </div>
+                </div>
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn22">$1000</button>
+                    </div>
+                </div>
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn23">$1000</button>
+                    </div>
+                </div>
+                <div class="box btn btn-fix text-center col-2 jeopardy-card">
+                    <div class="well card-box bg-blue">
+                        <button class="boxes" id="myBtn24">$1000</button>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+            <!-- scores and stuff -->
+            <div class="score col-2">
+                <h1>Score:
+                    <?= // Actual score var
+                    $_SESSION["score"]
+                    ?>
+                </h1>
 
-    <div onclick="answerQuestion();">
-        <div class="row">
-            <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                <div class="well card-box bg-blue">
-                    <button class="boxes" id="myBtn">$600</button>
+                <div class="exit">
+                    <a class="btn btn-danger" href="?command=over">End Game</a>
                 </div>
-            </div>
-            <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                <div class="well card-box bg-blue">
-                    <button class="boxes" id="myBtn">$600</button>
-                </div>
-            </div>
-            <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                <div class="well card-box bg-blue">
-                    <button class="boxes" id="myBtn">$600</button>
-                </div>
-            </div>
-            <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                <div class="well card-box bg-blue">
-                    <button class="boxes" id="myBtn">$600</button>
-                </div>
-            </div>
-            <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                <div class="well card-box bg-blue">
-                    <button class="boxes" id="myBtn">$600</button>
-                </div>
-            </div>
-        </div>
-    </div>
 
-    <div onclick="answerQuestion();">
-        <div class="row">
-            <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                <div class="well card-box bg-blue">
-                    <button class="boxes" id="myBtn">$800</button>
-                </div>
             </div>
-            <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                <div class="well card-box bg-blue">
-                    <button class="boxes" id="myBtn">$800</button>
-                </div>
-            </div>
-            <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                <div class="well card-box bg-blue">
-                    <button class="boxes" id="myBtn">$800</button>
-                </div>
-            </div>
-            <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                <div class="well card-box bg-blue">
-                    <button class="boxes" id="myBtn">$800</button>
-                </div>
-            </div>
-            <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                <div class="well card-box bg-blue">
-                    <button class="boxes" id="myBtn">$800</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <form method="post">
-        <div class="row">
-            <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                <div class="well card-box bg-blue">
-                    <button class="boxes" id="myBtn">$1000</button>
-                </div>
-            </div>
-            <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                <div class="well card-box bg-blue">
-                    <button class="boxes" id="myBtn">$1000</button>
-                </div>
-            </div>
-            <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                <div class="well card-box bg-blue">
-                    <button class="boxes" id="myBtn">$1000</button>
-                </div>
-            </div>
-            <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                <div class="well card-box bg-blue">
-                    <button class="boxes" id="myBtn">$1000</button>
-                </div>
-            </div>
-            <div class="box btn btn-fix text-center col-2 jeopardy-card">
-                <div class="well card-box bg-blue">
-                    <button class="boxes" id="myBtn">$1000</button>
-                </div>
-            </div>
-    </form>
-    </div>
-    </div>
-    <!-- scores and stuff -->
-    <div class="score col-2">
-        <h1>Score:
-            <?= // Actual score var
-            $_SESSION["score"]
-            ?>
-        </h1>
-
-        <div class="exit">
-            <a class="btn btn-danger" href="?command=over">End Game</a>
         </div>
 
-    </div>
-    </div>
-
-    <div id="myModal" class="modal">
-        <!-- Modal content -->
-        <div class="modal-content">
-        <form class="white" method="post">
-            <label for="guess" class="form-label" style="color:black"><?=$array[0]["question"]; ?></label>
-            <input type="text" class="form-control" id="guess" name="guess" /><br>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        <div id="myModal" class="modal">
+            <!-- Modal content -->
+            <div class="modal-content">
+                <form method="post">
+                    <label for="guess" class="form-label" style="color:black"><?= $array[0]["question"]; ?></label>
+                    <input type="text" class="form-control" id="guess" name="guess" /><br>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+        <div id="second" class="modal">
+            <!-- Modal content -->
+            <div class="modal-content">
+                <form method="post">
+                    <label for="guess" class="form-label" style="color:black"><?= $array[0]["question"]; ?></label>
+                    <input type="text" class="form-control" id="guess" name="guess" /><br>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
         </div>
 
-    </div>
-
-    <script>
-        function popUp() {
-            // Get the modal
-            var modal = document.getElementById("myModal");
-            // Get the button that opens the modal
-            var btn = document.getElementById("myBtn");
-            // Get the <span> element that closes the modal
-            var span = document.getElementsByClassName("close")[0];
-            // When the user clicks the button, open the modal 
-            btn.onclick = function() {
-                modal.style.display = "block";
-            }
-            // When the user clicks on <span> (x), close the modal
-            span.onclick = function() {
-                modal.style.display = "none";
-            }
-            // When the user clicks anywhere outside of the modal, close it
-            window.onclick = function(event) {
-                if (event.target == modal) {
-                    modal.style.display = "none";
-                }
-            }
-
-        }
-    </script>
-</body>
+    </body>
 
 </html>
